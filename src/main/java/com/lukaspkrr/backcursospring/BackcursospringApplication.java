98f90fd66dffe1a0df7525ab2b9f04a18b206e22
lukaspkrr <lucas10.mf@gmail.com>
2019-01-24 06:19:04 -0200

    Criação do projeto

---
+++ src/main/java/com/lukaspkrr/backcursospring/BackcursospringApplication.java
@@ -0,0 +1,14 @@
+package com.lukaspkrr.backcursospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackcursospringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackcursospringApplication.class, args);
+	}
+
+}
+
